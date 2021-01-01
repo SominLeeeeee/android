3baad4dd1c8e5549d1a90e016f591cc378991921
SoieJeong <soiejung@naver.com>
2021-01-01 22:23:20 +0900

    [AD-2] ViewPager2 fragment들

---
+++ app/src/main/java/com/petmeeting/zoocheck/DogRegisterAdapter.java
@@ -0,0 +1,33 @@
+package com.petmeeting.zoocheck;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class DogRegisterAdapter extends FragmentStateAdapter {
+    public int mCount;
+    public DogRegisterAdapter(@NonNull FragmentActivity fragmentActivity, int count) {
+        super(fragmentActivity);
+        mCount = count;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        int index = getRealPosition(position);
+        switch(index){
+            case 0 :
+                return DogProfileFrag.newInstance(index+1);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2000;
+    }
+
+    public int getRealPosition(int position){ return position % mCount;}
+}
